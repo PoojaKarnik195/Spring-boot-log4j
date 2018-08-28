468b23e4f031eb423272dd2a54bc731b49e27757
PoojaKarnik195 <41577579+PoojaKarnik195@users.noreply.github.com>
2018-08-28 17:52:39 +0530

    Add files via upload

---
+++ log4j2-example/src/test/java/com/cg/log4j2/log4j2example/Log4j2ExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cg.log4j2.log4j2example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Log4j2ExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
